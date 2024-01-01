7ef06beadd8cfadfbca6e06ed020615a46f24763
fjparrah <48214359+fjparrah@users.noreply.github.com>
2024-01-01 22:20:11 +0000

    actualizacion

--- nextjs-dashboard/app/ui/search.tsx
+++ nextjs-dashboard/app/ui/search.tsx
@@ -1,7 +1,7 @@
 'use client';
-
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 
 export default function Search({ placeholder }: { placeholder: string }) {
@@ -13,6 +13,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     console.log(`Searching... ${term}`);
 
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
